431819a605e750d7c2ba8e71bd7c5549a12856d5
mat3uslma <mateuslimama91@gmaill.com>
2023-06-08 09:27:31 -0300

    cadastro_dados

---
+++ main.py
@@ -0,0 +1,32 @@
+import  mysql.connector
+
+
+def cadastro():
+    print("Tela de Login ")
+    user = str(input("Usuario "))
+    email = str(input("Email "))
+    senha = str(input("Senha "))
+
+
+
+    conexao = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="098078",
+        database="login"
+
+    )
+    cursor = conexao.cursor()
+
+    comando = "INSERT INTO login_user (user,email,senha) VALUES (%s,%s,%s)"
+    dados = ((user),(email),(senha))
+    cursor.execute(comando,dados)
+    conexao.commit()
+
+
+    cursor.close()
+    conexao.close()
+
+
+cadastro()
+
